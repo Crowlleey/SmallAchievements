b30da0385ab16e3c4892b508002d82d530b5d98e
George Gomes <georgegomees@gmail.com>
2019-03-19 18:45:51 -0300

    Add firebase in project

---
+++ SmallAchievements/SmallAchievements/Networking/AuthManager.swift
@@ -0,0 +1,29 @@
+//
+//  AuthManager.swift
+//  SmallAchievements
+//
+//  Created by George on 19/03/2019.
+//  Copyright © 2019 crowcode. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+final class AuthManager {
+    
+    static func createAcc() {
+        
+    }
+    
+    static func login() {
+        
+    }
+    
+    static func forgotPassoword() {
+        
+    }
+    
+    static func logout() {
+        
+    }
+}
